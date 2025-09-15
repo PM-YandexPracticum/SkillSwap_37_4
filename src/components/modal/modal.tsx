cb4197e323c7b5e408739a90cb2751ad17ef6550
Kseniya Tsaruk <kseniya.tsaruk@yandex.ru>
2025-09-15 23:29:28 +0300

    Added closing Modal on esc and click on backdrop

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { FC, memo } from 'react';
+import { FC, memo, useEffect } from 'react';
 import { ReactNode } from 'react';
 import styles from './modal.module.css';
 
@@ -7,11 +7,36 @@ export type TModalProps = {
   children?: ReactNode;
 };
 
-export const Modal: FC<TModalProps> = memo(({ onClose, children }) => (
-  <>
-    <div className={styles.modal}>
-      <div className={styles.content}>{children}</div>
-    </div>
-    <div className={styles.overlay} onClick={onClose} />
-  </>
-));
+export const Modal: FC<TModalProps> = memo(({ onClose, children }) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleEscapeKey = (e: KeyboardEvent) => {
+      e.key === 'Escape' && onClose();
+    };
+    document.addEventListener('keydown', handleEscapeKey);
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.modal}>
+        <div className={styles.content}>{children}</div>
+      </div>
+      <div className={styles.overlay} onClick={handleBackdropClick} />
+    </>
+  );
+});
