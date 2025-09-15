81e48dd78ada2f13f7b38842ac1146dde029ff7d
romanyurkin2024 <tsoyroman2024@yandex.kz>
2025-09-16 02:23:22 +0500

    Сделал универсальный Input и UI поля для поиска SearchField

--- src/components/searchField/SearchField.tsx
+++ src/components/searchField/SearchField.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Search, X } from 'lucide-react';
 import styles from './SearchField.module.css';
+import { Input } from '../input/Input';
 
 interface SearchFieldProps {
   placeholder?: string;
@@ -10,31 +11,28 @@ export const SearchField: React.FC<SearchFieldProps> = ({
   placeholder = 'Искать навык'
 }) => {
   const [value, setValue] = useState('');
+  const searchInputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  const clearInput = () => {
-    setValue('');
-  };
+  const clearInput = () => setValue('');
 
   return (
-    <div className={styles.input__container}>
-      <Search style={{ color: '#69735D' }} />
-      <input
-        className={styles.input}
-        id='searchInput'
-        type='text'
-        value={value}
-        onChange={handleChange}
-        placeholder={placeholder}
-      />
-      {value && (
-        <button className={styles.clearButton} onClick={clearInput}>
-          <X style={{ color: '#253017' }} />
-        </button>
-      )}
-    </div>
+    <Input
+      ref={searchInputRef}
+      type='text'
+      value={value}
+      id='searchInput'
+      onChange={handleChange}
+      iconPaddingRight='32px'
+      placeholder={placeholder}
+      className={styles.search_input}
+      rightIconAriaLabel='Очистить поле поиска'
+      leftIcon={<Search className={styles.leftIcon} />}
+      onRightIconClick={clearInput}
+      rightIcon={value && <X className={styles.searchInputRightIcon} />}
+    />
   );
 };
