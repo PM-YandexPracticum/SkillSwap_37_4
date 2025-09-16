65dde7491a7c5c9514dd8f02e3aa5f3231f47f65
Jorgend <Fullsteck@yandex.kz>
2025-09-16 06:18:15 +0500

    fix FooterLinks

--- src/components/shared/ui/footerLinks/FooterLinks.tsx
+++ src/components/shared/ui/footerLinks/FooterLinks.tsx
@@ -1,38 +1,41 @@
+import { Link } from 'react-router-dom';
 import styles from './FooterLinks.module.css';
 
 export const FooterLinks = () => {
   return (
-        <div className={styles.section}>
-          <ul className={`${styles.linksList}`}>
-            <li>
-              <Link to="/about">О проекте</Link>
-            </li>
-            <li>
-              <Link to="/all_skills">Все навыки</Link>
-            </li>
-          </ul>
-        </div>
+    <>
+      <div className={styles.section}>
+        <ul className={`${styles.linksList}`}>
+          <li>
+            <Link to='/about'>О проекте</Link>
+          </li>
+          <li>
+            <Link to='/all_skills'>Все навыки</Link>
+          </li>
+        </ul>
+      </div>
 
-        <div className={styles.section}>
-          <ul className={styles.linksList}>
-            <li>
-              <Link to="/contacts">Контакты</Link>
-            </li>
-            <li>
-              <Link to="/blog">Блог</Link>
-            </li>
-          </ul>
-        </div>
+      <div className={styles.section}>
+        <ul className={styles.linksList}>
+          <li>
+            <Link to='/contacts'>Контакты</Link>
+          </li>
+          <li>
+            <Link to='/blog'>Блог</Link>
+          </li>
+        </ul>
+      </div>
 
-        <div className={styles.section}>
-          <ul className={styles.linksList}>
-            <li>
-              <Link to="/privacy">Политика конфиденциальности</Link>
-            </li>
-            <li>
-              <Link to="/terms">Пользовательское соглашение</Link>
-            </li>
-          </ul>
-        </div>
+      <div className={styles.section}>
+        <ul className={styles.linksList}>
+          <li>
+            <Link to='/privacy'>Политика конфиденциальности</Link>
+          </li>
+          <li>
+            <Link to='/terms'>Пользовательское соглашение</Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
