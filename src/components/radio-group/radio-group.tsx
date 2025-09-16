766717c0a7928c71c2fe2d634d4fa163558ac22d
Kseniya Tsaruk <kseniya.tsaruk@yandex.ru>
2025-09-16 12:10:06 +0300

    Added title

--- src/components/radio-group/radio-group.tsx
+++ src/components/radio-group/radio-group.tsx
@@ -11,12 +11,14 @@ export interface RadioGroupProps {
   items: RadioItem[];
   onChange: (id: string) => void;
   name: string;
+  title?: string;
 }
 
 export const RadioGroup: FC<RadioGroupProps> = ({
   items,
   onChange,
   name,
+  title,
 }) => {
   const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onChange(event.target.value);
@@ -24,6 +26,7 @@ export const RadioGroup: FC<RadioGroupProps> = ({
 
   return (
     <div className={styles.radioGroup}>
+      {title && <span className={styles.title}>{title}</span>}
       {items.map((item) => (
         <label key={item.id} className={styles.radioItem}>
           <input
