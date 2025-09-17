807631965e8828f2458b806a4240f4b38e3e715a
Valeriia Kharitonova <lerakharitonova2002@gmail.com>
2025-09-17 19:56:23 +0300

    Вынесла иконки, убрала кнопку, сделала весь компонент кликабельным

--- src/components/suggestNotification/suggestNotification.tsx
+++ src/components/suggestNotification/suggestNotification.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import styles from './suggest-notification.module.css';
-import { ReactComponent as LampIcon } from './lamp.svg';
-import { ReactComponent as CloseIcon } from './cross.svg';
+import { ReactComponent as LampIcon } from '../app/assets/static/icons/lamp.svg';
+import { ReactComponent as CloseIcon } from '../app/assets/static/icons/cross.svg';
 type suggestNotificationProps = {
   senderName: string;
   onClick: () => void;
@@ -15,23 +15,18 @@ export const SuggestNotificationUI: FC<suggestNotificationProps> = ({
 }) => {
   return (
     <>
-      <div className={styles.card}>
+      <div className={styles.card} onClick={onAction}>
         <div className={styles.left}>
           <LampIcon className={styles.icon} />
           <h3 className={styles.title}>{senderName} предлагает вам обмен</h3>
         </div>
-        <button
-          className={styles.action}
-          onClick={onAction}
-          aria-label='Перейти к предложению'
-        >
-          Перейти
-        </button>
-
-        {/* Крестик поверх всего */}
+        
         <button
           className={styles.close}
-          onClick={onClick}
+          onClick={(e) => {
+            e.stopPropagation(); 
+            onClick();
+          }}
           aria-label='Закрыть уведомление'
         >
           <CloseIcon />
