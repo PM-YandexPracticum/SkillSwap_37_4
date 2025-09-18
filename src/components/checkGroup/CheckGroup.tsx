c7014c962212ca0afa6860abc99ee6d8a03d77d0
Shelepov Sergey <shelepov-sa@yandex.ru>
2025-09-18 15:22:57 +0300

    feat(ui/checkbox): стили и поведение по макету; rename dir (fix Cyrillic); update SVG icons (stroke, white); demo removed from App.tsx

--- src/components/checkGroup/CheckGroup.tsx
+++ src/components/checkGroup/CheckGroup.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Checkbox } from '../ui/сheckbox/Checkbox';
+import { Checkbox } from '../ui/checkbox/Checkbox';
 import style from "./CheckGroup.module.css"
 
 export type CheckGroupItem = {
@@ -30,6 +30,7 @@ export const CheckGroup: FC<CheckGroupProps> = ({
           label={item.label}
           checked={!!checkedItems[item.id]}
           onChange={onChange}
+          variant={nameGroup.toLowerCase().includes('subcategory') ? 'check' : 'minus'}
         />
       ))}
     </div>
