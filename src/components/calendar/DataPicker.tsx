8a51932950a76813d9c32c1023ffa98aafc1c0ff
erges699 <erges699@gmail.com>
2025-09-22 13:26:17 +0300

    DataPicker&Css 20250922-02

--- src/components/calendar/DataPicker.tsx
+++ src/components/calendar/DataPicker.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
-import { Dropdown } from '../DropDown';
+import { DropDown } from '../DropDown';
+import { GreenButton } from '../buttons/GreenButton/GreenButton';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import { ru } from 'date-fns/locale';
 import styles from './DataPicker.module.css';
@@ -30,10 +31,91 @@ export const DatePickerComponent = ({ onDateChange }: DatePickerProps) => {
     ref.current?.setOpen(false);
   };
 
+  const currentYear = new Date().getFullYear();
+
+  const months = Array.from({ length: 12 }, (_, i) => ({
+    value: i.toString(),
+    label: new Date(0, i).toLocaleString('ru', { month: 'long' })
+  }));
+
+  const years = Array.from({ length: 100 }, (_, i) => {
+    const year = currentYear - i;
+    return { value: year.toString(), label: year.toString() };
+  });
+
   return (
     <div className={styles.wrapper}>
       <label className={styles.label}>Дата рождения</label>
-      <DatePicker showIcon selected={selectedDate} onChange={handleChange} />
+      <DatePicker
+        ref={ref}
+        showIcon
+        selected={selectedDate}
+        onChange={handleChange}
+        locale='ru'
+        maxDate={new Date()}
+        dateFormat='dd.MM.yyyy'
+        shouldCloseOnSelect={false}
+        calendarStartDay={1}
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode='select'
+        className={styles.input}
+        renderCustomHeader={({ date, changeYear, changeMonth }) => {
+          const selectedMonth = months.find(
+            (m) => m.value === date.getMonth().toString()
+          );
+          const selectedYear = years.find(
+            (y) => y.value === date.getFullYear().toString()
+          );
+
+          return (
+            <div className={styles['custom-header']}>
+              <DropDown
+                options={months}
+                value={selectedMonth?.label}
+                onChange={(option) => {
+                  if (!Array.isArray(option)) {
+                    changeMonth(Number(option.valueOf));
+                  }
+                }}
+                placeholder='Месяц'
+              />
+
+              <DropDown
+                options={years}
+                value={selectedYear?.label}
+                onChange={(option) => {
+                  if (!Array.isArray(option)) {
+                    changeYear(Number(option.valueOf));
+                  }
+                }}
+                placeholder='Год'
+              />
+            </div>
+          );
+        }}
+        calendarContainer={({ children }) => (
+          <div className={styles['calendar-container']}>
+            {children}
+            <div className={styles.footer}>
+              <GreenButton
+                onClick={handleCancel}
+                type='button'
+                className='disabled'
+              >
+                Отменить
+              </GreenButton>
+              <GreenButton
+                onClick={handleConfirm}
+                type='button'
+                className='active'
+              >
+                Выбрать
+              </GreenButton>
+            </div>
+          </div>
+        )}
+      />
     </div>
   );
 };
