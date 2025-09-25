feb27f75cb4c0c090629d9cfd3c6c7c5c5c396c8
romanyurkin2024 <tsoyroman2024@yandex.kz>
2025-09-25 17:47:42 +0500

    Добавил слайс userSlice

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../slices/userSlice/userSlice'; // путь к твоему слайсу
 
 import {
   TypedUseSelectorHook,
@@ -6,7 +7,9 @@ import {
   useSelector as selectorHook
 } from 'react-redux';
 
-const rootReducer = {};
+const rootReducer = {
+  'user': userReducer
+};
 
 const store = configureStore({
   reducer: rootReducer,
