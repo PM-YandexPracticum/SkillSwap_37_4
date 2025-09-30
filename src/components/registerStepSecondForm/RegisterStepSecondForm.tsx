2fd60d80809e4eb909f6c501d1272df049fbafe6
erges699 <erges699@gmail.com>
2025-09-30 13:07:09 +0300

    202530-01

--- src/components/registerStepSecondForm/RegisterStepSecondForm.tsx
+++ src/components/registerStepSecondForm/RegisterStepSecondForm.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect, memo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../services/store/store';
 import iconPlus from '../app/assets/static/iconsUi/add.svg';
+import genderData from '../../const/gender.json';
+import townsData from '../../const/towns.json';
 import { Input } from '../../components/input/Input';
 import { DatePickerComponent } from '../../components/calendar/DataPicker';
 import { DropDown } from '../../components/DropDown';
@@ -153,104 +155,92 @@ export const RegisterStepSecondForm = memo(
     };
 
     return (
-      <form className={styles.form_container}>
-        <div className={styles.icon__container}>
-          <label htmlFor='avatar' className={styles.avatar__label}>
-            <img className={styles.avatar__label_plusIcon} src={iconPlus} />
-          </label>
-          <input
-            type='file'
-            name='avatar'
-            id='avatar'
-            className={styles.avatar__input}
-            multiple={false}
-            onChange={handleFileChange}
-            aria-label='Загрузить аватар'
-          />
-          {formData.avatar && (
-            <img
-              src={typeof formData.avatar === 'string' ? formData.avatar : ''}
-              alt='Аватар пользователя'
+      <div className={styles.page_container}>
+        <form className={styles.form_container}>
+          <div className={styles.icon__container}>
+            <label htmlFor='avatar' className={styles.avatar__label}>
+              <img className={styles.avatar__label_plusIcon} src={iconPlus} />
+            </label>
+            <input
+              type='file'
+              name='avatar'
+              id='avatar'
+              className={styles.avatar__input}
+              multiple={false}
+              onChange={handleFileChange}
+              aria-label='Загрузить аватар'
             />
-          )}
-        </div>
-        <div className={styles.element__input}>
+            {formData.avatar && (
+              <img
+                src={typeof formData.avatar === 'string' ? formData.avatar : ''}
+                alt='Аватар пользователя'
+              />
+            )}
+          </div>
+
           <Input
             label='Имя'
             value={formData.name}
             placeholder='Введите ваше имя'
             onChange={handleChange}
+            className={styles.element__input}
           />
-        </div>
-        <div className={styles.date__input}>
-          <DatePickerComponent selectedDate={date} setSelectedDate={setDate} />
-        </div>
+          <div className={styles.input__drops_container}>
+            <div className={styles.input__drops_birthday}>
+              <DatePickerComponent
+                selectedDate={date}
+                setSelectedDate={setDate}
+              />
+            </div>
 
-        <div className={styles.fixed__input}>
-          <select
-            name='gender'
-            id='gender'
-            title='Пол'
-            value={formData.gender}
-            onChange={handleChange}
-          >
-            <option value='Не указан'>Не указан</option>
-            <option value='Мужской'>Мужской</option>
-            <option value='Женский'>Женский</option>
-          </select>
-        </div>
-
-        <div className={styles.element__input}>
-          <select
-            name='city'
-            id='city'
-            title='Город'
-            value={formData.city}
-            onChange={handleChange}
-          >
-            <option value='Не указан'>Не указан</option>
-            {/* Здесь должен быть список городов */}
-          </select>
-        </div>
-
-        <div className={styles.element__input}>
+            <DropDown
+              options={genderData}
+              /*onChange={handleChange}*/
+              placeholder='Не указан'
+              className={styles.input__drops_gender}
+            />
+          </div>
+          <DropDown
+            options={townsData}
+            /*onChange={handleChange}*/
+            placeholder='Не указан'
+            className={styles.input__drops_town}
+          />
           <DropDown
             placeholder='Выберите категорию'
             options={categoryOptions}
             value={selectedCategories}
             onChange={handleCategoryChange}
+            className={styles.input__drops_town}
           />
-        </div>
-
-        <div className={styles.element__input}>
           <DropDown
             placeholder='Выберите подкатегорию'
             options={subcategoryOptions}
             value={selectedSubcategories[0] || ''}
             onChange={handleSubcategoryChange}
+            className={styles.input__drops_town}
           />
           {isContinueButtonDisabled && (
             <p className={styles.error}>Сначала выберите категорию</p>
           )}
-        </div>
-
-        <div className={styles.button__group}>
-          <GreenBorderButton
-            className={styles.button_width}
-            onClick={handleBack}
-            type='submit'
-          >
-            {buttonPrevText}
-          </GreenBorderButton>
-          <GreenButton
-            className={styles.button_width}
-            onClick={handleNext}
-            type='submit'
-          >
-            {buttonNextText}
-          </GreenButton>
-        </div>
-      </form>
+          <div className={styles.button__group}>
+            <GreenBorderButton
+              className={styles.button_width}
+              onClick={handleBack}
+              type='submit'
+            >
+              {buttonPrevText}
+            </GreenBorderButton>
+            <GreenButton
+              className={styles.button_width}
+              onClick={handleNext}
+              type='submit'
+            >
+              {buttonNextText}
+            </GreenButton>
+          </div>
+        </form>
+      </div>
     );
   }
 );
