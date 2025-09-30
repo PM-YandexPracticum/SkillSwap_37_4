f119d6cc1b1ab4f0d8fbea41dc616b97c18a8530
Kseniya Tsaruk <kseniya.tsaruk@yandex.ru>
2025-09-30 22:43:47 +0300

    подключен UserSlice к /profile/details

--- src/components/profilePageAvatar/ProfilePageAvatar.tsx
+++ src/components/profilePageAvatar/ProfilePageAvatar.tsx
@@ -1,11 +1,31 @@
 import styles from './ProfilePageAvatar.module.css';
+import { TUser } from '../../services/slices/userSlice/type';
+import { useRef, ChangeEvent, useState, SyntheticEvent } from 'react';
+import { TSkillTag } from "../../components/cardTag/CardTag"
 
-export function ProfilePageAvatar() {
+export type ProfilePageFormProps = {
+  user: {
+    id: number | undefined,
+    name: string | undefined,
+    email: string | undefined,
+    password: string | undefined,
+    avatarUrl: string | undefined,
+    birthday: Date | undefined,
+    aboutMe: string | undefined,
+    city: string | undefined,
+    gender: string | undefined,
+    wantLearn: TSkillTag[] | undefined, 
+    canLearn: TSkillTag[] | undefined
+  }
+  handleInputChange: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void;
+};
+
+export function ProfilePageAvatar({ user, handleInputChange }: ProfilePageFormProps) {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
   return (
     <div className={styles.profileAvatarBlock}>
       <img
-        //src={avatarPreview || '/default-avatar.png'}
-        src={'/default-avatar.png'}
+        src={user?.avatarUrl}
         alt='Аватар'
         className={styles.profileAvatar}
         onError={(e) => {
@@ -15,14 +35,28 @@ export function ProfilePageAvatar() {
       />
       <input
         type='file'
-        /*ref={fileInputRef}
-        onChange={handleAvatarChange}*/
+        ref={fileInputRef}
         accept='image/*'
         style={{ display: 'none' }}
+        onChange={(e) => {
+        const file = e.target.files?.[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+          handleInputChange({
+            target: {
+              name: "avatarUrl",
+              value: reader.result, 
+            },
+          } as any);
+          };
+          reader.readAsDataURL(file);
+        }
+      }}
       />
       <button
         className={styles.profileEditPhotoBtn}
-        /*onClick={() => fileInputRef.current?.click()}*/
+        onClick={() => fileInputRef.current?.click()}
         type='button'
       >
         <span className={styles.profileGalleryEdit} />
